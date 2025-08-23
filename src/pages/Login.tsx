b8a62159a4d89904763731fa89fdcae0c5f6b7df
codemonk61 <codemonk61@gmail.com>
2025-08-23 21:51:52 +0530

    login User field corrected

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,20 +1,28 @@
 import Button from "../components/Button/Button"
 import Card from "../components/card/Card"
 import Input from "../components/Input/Input"
-import React from "react"
+import React, { useState } from "react"
 
 const Login = () => {
-
+    const [name, setName] = useState<string>('')
     const [password, setPassword] = React.useState<string>("")
+    
 
     const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
       const inputValue = e.target.value
       setPassword(inputValue)
+      
+    }
+
+    const handleName = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setName(e.target.value)
     }
 
     const handleLogin = () => {
-      console.log('login clicked')
+      console.log('login clicked') 
     }
+
+    
 
   return (
     <div className='flex justify-center items-center h-screen' > 
@@ -23,8 +31,8 @@ const Login = () => {
         <h1 className='text-3xl text-center mb-3 font-bold text-gray-600 '>Login</h1>
        
         <Input
-         handleInput ={handleInput}
-         value={password}
+         handleInput ={handleName}
+         value={name}
          placeholder="Enter your username"
          title={"User Name"}
          required={true}
