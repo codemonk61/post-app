2d07a1eb8278fc403729f3c93c98f7b255fc647e
codemonk61 <codemonk61@gmail.com>
2025-08-24 12:20:11 +0530

    changes mobile type

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,8 +1,42 @@
+import { useState } from "react";
+import Card from "../components/card/Card";
+import Input from "../components/Input/Input";
 
 const SignUp = () => {
-  return (
-    <div>SignUp</div>
-  )
-}
+  const [username, setUserName] = useState<string>('')
+  const [mobileNo, setMobileNo] = useState<string>('')
 
-export default SignUp
+  const handleUserName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUserName(e.target.value);
+  }
+
+  const handleMobileNo = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setMobileNo((e.target.value))
+  }
+
+  return (
+    <div className='flex justify-center items-center h-screen'>
+      <Card>
+        <Input
+        handleInput = {handleUserName}
+        placeholder={'username'}
+        value={username}
+        title={"username"}
+        required= {true}
+        />
+
+        <Input
+        handleInput = {handleMobileNo}
+        placeholder={'Mobile No'}
+        value={mobileNo}
+        title={"Mobile No:"}
+        required= {true}
+        />
+        
+      </Card>
+      
+    </div>
+  );
+};
+
+export default SignUp;
